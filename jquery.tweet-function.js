e52eb98e93125e0ef6bc03df099e3adb40d45ba7
goana <asgomiz@gmail.com>
2013-02-14 16:12:03 +0100

    Update jquery.tweet-function.js

--- jquery.tweet-function.js
+++ jquery.tweet-function.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-        $(".tweet").tweet({ // aca es donde se le indica donde poner los tweets, le pondremos un div vacio con clase .tweets
+        $(".tweet").tweet({ // Indicamos donde poner los tweets, le pondremos un div vacio con clase .tweets
             username: "pensandoenweb", // el usuario
             avatar_size: 0, // Ponle 0 si no quieres avatares
             count: 2, // Numero de tweets
